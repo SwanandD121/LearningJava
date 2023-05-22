26337629badf47d5e6be95aa26b5336c0ca50abb
Swanand Deshpande <swananddeshpande121@gmail.com>
2023-05-22 22:31:49 +0530

    Second commit

---
+++ src/main2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class main2{
+
+    public static void main(String[] args){
+        
+        // Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("Whats your name?");
+        // String name = scanner.nextLine();
+
+        // System.out.println("Whats your age?");
+        // int age = scanner.nextInt();
+        // scanner.nextLine();                        /*  scanner.nextInt jo hai wo pure scanner ko clear nahi kar pata 
+        //                                            isliye ye line likhni padti taki scanner clear ho jaye aur next value lele     */
+        // System.out.println("Whats ur favourite food?");
+        // String food = scanner.nextLine();
+
+        // System.out.println("Hi " + name);
+        // System.out.println("You are " + age + " years old");
+        // System.out.println("You like " + food);
+
+        double friends = 10;
+
+        friends = (double) friends / 3;
+
+        System.out.println(friends);
+    }
+}
